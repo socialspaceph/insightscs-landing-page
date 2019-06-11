a487b484f8364b5169d3d957fe7f0ffb73c55fb0
James Raphael Vibar <vibarjamess@gmail.com>
2019-06-11 17:33:06 +0800

    add icons

---
+++ icons/MessageBubble.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function FacebookIcon() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 48 48"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M40 4H8C5.8 4 4 5.8 4 8V44L12 36H40C42.2 36 44 34.2 44 32V8C44 5.8 42.2 4 40 4ZM40 32H10.34L8 34.34V8H40V32ZM18 18H14V22H18V18ZM30 18H34V22H30V18ZM26 18H22V22H26V18Z"
+        fill="inherit"
+      />
+    </svg>
+  )
+}
--- icons/index.js
+++ icons/index.js
@@ -1,4 +1,9 @@
 import FacebookIcon from './FacebookIcon'
 import ChevronRight from './ChevronRight'
+import Heart from './Heart'
+import Mail from './Mail'
+import MessageBubble from './MessageBubble'
+import Rotate from './Rotate'
+import Wave from './Wave'
 
-export { FacebookIcon, ChevronRight }
+export { Wave, Rotate, MessageBubble, Mail, Heart, FacebookIcon, ChevronRight }
