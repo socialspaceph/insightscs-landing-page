8fe1e6578761d5edb6aba4b426efe830e6af1e37
James Raphael Vibar <vibarjamess@gmail.com>
2019-06-11 15:14:10 +0800

    add chevron right icon

---
+++ icons/ChevronRight.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ChevronRight() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.70501 6L8.29501 7.41L12.875 12L8.29501 16.59L9.70501 18L15.705 12L9.70501 6Z"
+        fill="inherit"
+      />
+    </svg>
+  )
+}
